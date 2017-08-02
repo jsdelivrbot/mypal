ca4633415f5ea0a7dc84fabb0581f7359e879688
kitsonlai <kitsonlaioz@gmail.com>
2017-08-02 13:38:29 +1000

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -53,13 +53,18 @@
                                 <button type="submit" class="btn btn-primary">
                                     Login
                                 </button>
-                                <a class="btn btn-success" href="/register" role="button">Register</a>
                                 <a class="btn btn-link" href="{{ route('password.request') }}">
                                     Forgot Your Password?
-                                </a                                
+                                </a>                                
                             </div>
                         </div>
                     </form>
+                    <hr>
+                    <div class="form-group">
+                    <div class="col-md-8 col-md-offset-4">
+                    <a class="btn btn-success" href="/register" role="button">Create your MyPal account</a>
+                    </div>
+                    </div>
                 </div>
             </div>
         </div>
